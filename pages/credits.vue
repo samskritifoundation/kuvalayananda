b31084ae460dabd811be047bc95b3abe1e0be2e0
pankaja-shree <pankajashree@gmail.com>
2018-11-09 16:40:57 +0530

    create credits page

---
+++ pages/credits.vue
@@ -0,0 +1,3 @@
+<template>
+    <a href="https://sa.wikisource.org/wiki/%E0%A4%95%E0%A5%81%E0%A4%B5%E0%A4%B2%E0%A4%AF%E0%A4%BE%E0%A4%A8%E0%A4%A8%E0%A5%8D%E0%A4%A6%E0%A4%83_%E0%A4%B8%E0%A5%8B%E0%A4%A6%E0%A4%BE%E0%A4%B9%E0%A4%B0%E0%A4%A3%E0%A4%AE%E0%A5%8D">Source for Sanskrit Text</a>
+</template>
